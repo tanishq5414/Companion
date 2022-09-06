817f59bd3b6abb2a71d11adc29ef6c3e30c6a13b
Tanishq Agarwal <tanishqagarwal5414@gmail.com>
2022-09-06 21:26:06 +0530

    added few more changes to homepage

--- lib/home.dart
+++ lib/home.dart
@@ -390,135 +390,209 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Column recentBuilder(Size size) {
-    return Column(
-      children: [
-        TextButton(
-          style: ElevatedButton.styleFrom(
-            minimumSize: Size.zero,
-            padding: EdgeInsets.zero,
-            onPrimary: Colors.grey,
-            primary: Colors.white,
-          ),
-          onPressed: () {},
-          child: SizedBox(
-            width: size.width * 0.34,
-            height: size.width * 0.34,
-            child: Column(
-              children: [
-                Container(
-                  width: size.width * 0.34,
-                  height: size.width * 0.34,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('lib/assets/pictures/black.jpg'),
-                      fit: BoxFit.cover,
+ 
+  Container trendingBuilder(Size size) {
+    return Container(
+      width: size.width * 0.34,
+      child: Column(
+        children: [
+          TextButton(
+            style: ElevatedButton.styleFrom(
+              minimumSize: Size.zero,
+              padding: EdgeInsets.zero,
+              onPrimary: Colors.grey,
+              primary: Colors.white,
+            ),
+            onPressed: () {},
+            child: SizedBox(
+              width: size.width * 0.34,
+              height: size.width * 0.34,
+              child: Column(
+                children: [
+                  Container(
+                    width: size.width * 0.34,
+                    height: size.width * 0.34,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('lib/assets/pictures/black.jpg'),
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          height: size.height * 0.01,
-        ),
-        Text(
-          'Automata Compiler',
-          style: const TextStyle(
-              fontSize: 13, fontWeight: FontWeight.bold, color: Colors.black),
-              overflow: TextOverflow.fade,
-        ),
-      ],
+          SizedBox(
+            height: size.height * 0.01,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                'Unit 5',
+                style: TextStyle(
+                    fontSize: 13, fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+              Text('v4.0',
+                  style: TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey)),
+            ],
+          ),
+          SizedBox(
+            height: size.height * 0.007,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: const [
+              Text(
+                'Data Mining',
+                style: TextStyle(
+                    fontSize: 11, color: Colors.black),
+                    overflow: TextOverflow.fade,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
-  Column bookBuilder(Size size) {
-    return Column(
-      children: [
-        TextButton(
-          style: ElevatedButton.styleFrom(
-            minimumSize: Size.zero,
-            padding: EdgeInsets.zero,
-            onPrimary: Colors.grey,
-            primary: Colors.white,
-          ),
-          onPressed: () {},
-          child: SizedBox(
-            width: size.width * 0.34,
-            height: size.width * 0.34,
-            child: Column(
-              children: [
-                Container(
-                  width: size.width * 0.34,
-                  height: size.width * 0.34,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('lib/assets/pictures/black.jpg'),
-                      fit: BoxFit.cover,
+  Container recentBuilder(Size size) {
+    return Container(
+      width: size.width * 0.34,
+      child: Column(
+        children: [
+          TextButton(
+            style: ElevatedButton.styleFrom(
+              minimumSize: Size.zero,
+              padding: EdgeInsets.zero,
+              onPrimary: Colors.grey,
+              primary: Colors.white,
+            ),
+            onPressed: () {},
+            child: SizedBox(
+              width: size.width * 0.34,
+              height: size.width * 0.34,
+              child: Column(
+                children: [
+                  Container(
+                    width: size.width * 0.34,
+                    height: size.width * 0.34,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('lib/assets/pictures/black.jpg'),
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          height: size.height * 0.01,
-        ),
-        Container(
-          width: size.width * 0.34,
-          child: Text(
-            'DataBase Management System',
-
-            style: const TextStyle(
-                fontSize: 13, fontWeight: FontWeight.bold, color: Colors.black),
-                overflow: TextOverflow.fade,
+          SizedBox(
+            height: size.height * 0.01,
           ),
-        ),
-      ],
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                'Unit 1',
+                style: TextStyle(
+                    fontSize: 13, fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+              Text('v1.0',
+                  style: TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey)),
+            ],
+          ),
+          SizedBox(
+            height: size.height * 0.007,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: const [
+              Text(
+                'Web Technologies',
+                style: TextStyle(
+                    fontSize: 11, color: Colors.black),
+                    overflow: TextOverflow.fade,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
-
-  Column trendingBuilder(Size size) {
-    return Column(
-      children: [
-        TextButton(
-          style: ElevatedButton.styleFrom(
-            minimumSize: Size.zero,
-            padding: EdgeInsets.zero,
-            onPrimary: Colors.grey,
-            primary: Colors.white,
-          ),
-          onPressed: () {},
-          child: SizedBox(
-            width: size.width * 0.34,
-            height: size.width * 0.34,
-            child: Column(
-              children: [
-                Container(
-                  width: size.width * 0.34,
-                  height: size.width * 0.34,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('lib/assets/pictures/black.jpg'),
-                      fit: BoxFit.cover,
+  Container bookBuilder(Size size) {
+    return Container(
+      width: size.width * 0.34,
+      child: Column(
+        children: [
+          TextButton(
+            style: ElevatedButton.styleFrom(
+              minimumSize: Size.zero,
+              padding: EdgeInsets.zero,
+              onPrimary: Colors.grey,
+              primary: Colors.white,
+            ),
+            onPressed: () {},
+            child: SizedBox(
+              width: size.width * 0.34,
+              height: size.width * 0.34,
+              child: Column(
+                children: [
+                  Container(
+                    width: size.width * 0.34,
+                    height: size.width * 0.34,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('lib/assets/pictures/black.jpg'),
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          height: size.height * 0.01,
-        ),
-        Text(
-          'Web Technologies',
-          style: const TextStyle(
-              fontSize: 13, fontWeight: FontWeight.bold, color: Colors.black),
-              overflow: TextOverflow.fade,
-        ),
-      ],
+          SizedBox(
+            height: size.height * 0.01,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                'Unit 3',
+                style: TextStyle(
+                    fontSize: 13, fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+              Text('v3.0',
+                  style: TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey)),
+            ],
+          ),
+          SizedBox(
+            height: size.height * 0.007,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: const [
+              Text(
+                'DBMS',
+                style: TextStyle(
+                    fontSize: 11, color: Colors.black),
+                    overflow: TextOverflow.fade,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
