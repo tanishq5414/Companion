579ae725060612703e59200f0ef51ad4bda35141
Tanishq Agarwal <tanishqagarwal5414@gmail.com>
2023-01-01 17:54:20 +0530

    fixed null error in advertisments

--- lib/features/components/advertisment.dart
+++ lib/features/components/advertisment.dart
@@ -7,39 +7,46 @@ import 'package:url_launcher/url_launcher.dart';
 List<Advertisment> advertlist = [];
 ListView advertismentBuilder(Size size, context, WidgetRef ref) {
   final advert = ref.watch(advertismentDataProvider);
-  advert.when(
-    data: (advert) {
-      advertlist = advert.map((e) => e).toList();
-    },
-    error: ((error, stackTrace) => Text(error.toString())),
-    loading: () => const Center(
-      child: CircularProgressIndicator(
-        color: Colors.white,
+  getadvert() {
+    advert.when(
+      data: (advert) {
+        advertlist = advert.map((e) => e).toList();
+      },
+      error: ((error, stackTrace) => Text(error.toString())),
+      loading: () => const Center(
+        child: CircularProgressIndicator(
+          color: Colors.white,
+        ),
       ),
-    ),
-  );
-  _launchURL(String url) async {
-  final uri = Uri.parse(url);
-  if (await canLaunchUrl(uri)) {
-    await launch(url);
-  } else {
-    throw 'Could not launch $url';
+    );
+    return advertlist;
   }
-}
+    _launchURL(String url) async {
+      final uri = Uri.parse(url);
+      if (await canLaunchUrl(uri)) {
+        await launch(url);
+      } else {
+        throw 'Could not launch $url';
+      }
+  }
+
   return ListView.builder(
-      itemCount: 1,
+      itemCount: getadvert().length,
       physics: const NeverScrollableScrollPhysics(),
       shrinkWrap: true,
       itemBuilder: (context, index) {
-        String image = advertlist[0].photoUrl??"";
-        String url = advertlist[0].redirectLink;
+        var advertlist = getadvert();
+        String image = advertlist[index].photoUrl;
+        String url = advertlist[index].redirectLink;
         // print  (usercourses);
         return InkWell(
           onTap: () => _launchURL(url),
           child: SizedBox(
               width: size.width,
               height: size.width * 0.7,
-              child: ClipRRect(borderRadius: BorderRadius.circular(20.0),child: Image.network(image))),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20.0),
+                  child: Image.network(image))),
         );
       });
 }
