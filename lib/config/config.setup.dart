8b602e9d8e239a68b88d1a10edc6551f36450793
Tanishq Agarwal <tanishqagarwal5414@gmail.com>
2023-06-03 22:36:59 +0530

    added config.setup file for setting up the repo

---
+++ lib/config/config.setup.dart
@@ -0,0 +1,16 @@
+// API URLs
+// rename the file to config.dart and add your API URLs here
+const apiUrl = ""; // Insert the URL of your hosted Node server here [server link ]
+
+const supabaseApiURL = ""; // Insert your Supabase API URL here
+const supabaseApiPublicKey = ""; // Insert your Supabase API public key here
+
+// Endpoint URLs
+const coursesUrl = '$apiUrl/getJsonFiles'; // URL for fetching JSON files
+const notesUrl = '$apiUrl/getFiles'; // URL for fetching files
+const uploadNotesUrl = '$apiUrl/addFile'; // URL for uploading files
+const trendingNotesByDayURL = '$apiUrl/getTrendingDay'; // URL for fetching trending notes by day
+const trendingNotesByWeekURL = '$apiUrl/getTrendingWeek'; // URL for fetching trending notes by week
+const trendingNotesDataURL = '$apiUrl/trending'; // URL for fetching all trending notes
+const advertismentURL = 'https://tanishq5414.github.io/apiNotesApp/test1.json'; // URL for advertisements
+const wordlistURL = 'https://tanishq5414.github.io/apiNotesApp/english.json'; // URL for wordlist
